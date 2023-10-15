dd78eae2f18ed7174dd374f4d29f012d4e46fa51
Fernando de Paz <f.depaz@hotmail.com>
2023-10-15 10:13:39 -0700

    woof

--- src/main/java/bytesandbots/custom/customrules/Main.java
+++ src/main/java/bytesandbots/custom/customrules/Main.java
@@ -650,6 +650,34 @@ public final class Main extends JavaPlugin {
     		meta.setDisplayName(ChatColor.RESET + "Compacted Diamond");
     		item.setItemMeta(meta);
     		player.getInventory().addItem(item);
+    		
+    		
+    		ItemStack item2 = new ItemStack(Material.DIAMOND_SWORD);
+    		ItemMeta meta2 = item2.getItemMeta();
+    		meta2.setCustomModelData(1);
+    		meta2.setDisplayName(ChatColor.RED + "DarkBane");
+    		List<String> lore = new ArrayList<>();
+    		lore.add("Sweeping Edge X");
+    		meta2.setLore(lore);
+    		
+    		AttributeModifier attackSpeed = new AttributeModifier(UUID.randomUUID(),"generic.attack_speed",5,AttributeModifier.Operation.ADD_NUMBER,EquipmentSlot.HAND);
+    		meta2.addAttributeModifier(Attribute.GENERIC_ATTACK_SPEED,attackSpeed);
+    		
+    		AttributeModifier armor = new AttributeModifier(UUID.randomUUID(),"generic.armor",20,AttributeModifier.Operation.ADD_NUMBER,EquipmentSlot.HAND);
+    		meta2.addAttributeModifier(Attribute.GENERIC_ARMOR,armor);
+    		
+    		AttributeModifier attackDamage = new AttributeModifier(UUID.randomUUID(),"generic.attack_damage",54,AttributeModifier.Operation.ADD_NUMBER,EquipmentSlot.HAND);
+    		meta2.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE,attackDamage);
+    		
+    		AttributeModifier maxHealth = new AttributeModifier(UUID.randomUUID(),"generic.max_health",10,AttributeModifier.Operation.ADD_NUMBER,EquipmentSlot.HAND);
+    		meta2.addAttributeModifier(Attribute.GENERIC_MAX_HEALTH,maxHealth);
+    		
+    		AttributeModifier movementSpeed = new AttributeModifier(UUID.randomUUID(),"generic.movement_speed",.3,AttributeModifier.Operation.ADD_NUMBER,EquipmentSlot.HAND);
+    		meta2.addAttributeModifier(Attribute.GENERIC_MOVEMENT_SPEED,movementSpeed);
+    		
+    		
+    		item2.setItemMeta(meta2);
+    		player.getInventory().addItem(item2);
     	
           	return true;
     	}
